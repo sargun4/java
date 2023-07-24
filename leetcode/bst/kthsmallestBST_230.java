cba9d484fd60f8fc8a8faf61d10c20f22f3779e5
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-07-24 19:49:25 +0530

    Create kthsmallestBST_230.java

---
+++ leetcode/bst/kthsmallestBST_230.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+        return root.val;
+    }
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
